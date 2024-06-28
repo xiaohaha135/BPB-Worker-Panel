371a10faab0f4f500d90b3d097800176dc114eb9
xiaohaha135 <130602225+xiaohaha135@users.noreply.github.com>
2024-06-28 23:25:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -823,18 +823,18 @@ const generateRemark = (index, port) => {
     switch (index) {
         case 0:
         case 1:
-            remark = `💦VT网云_${index + 1} : ${port}`;
+            remark = `💦VT网云_${index + 1}`;
             break;
         case 2:
         case 3:
-            remark = `💦网云v4_${index - 1} : ${port}`;
+            remark = `💦网云v4_${index - 1}`;
             break;
         case 4:
         case 5:
-            remark = `💦网云v6_${index - 3} : ${port}`;
+            remark = `💦网云v6_${index - 3}`;
             break;
         default:
-            remark = `💦网云优选_${index - 5} : ${port}`;
+            remark = `💦网云优选_${index - 5}`;
             break;
     }
 
