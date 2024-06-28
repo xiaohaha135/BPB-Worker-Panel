8327cd86a1aeec41652f5b51215e53868666a3e8
xiaohaha135 <130602225+xiaohaha135@users.noreply.github.com>
2024-06-28 23:52:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '88f9d6cb-a3d5-492d-cbc5-afbf2118bd5c';
+let userID = '99252742-58f6-40be-853f-c3d5dfc26a82';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
