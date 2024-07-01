0d19d6502cc604d03eb4c1741542b64c09f1ffec
xiaohaha135 <130602225+xiaohaha135@users.noreply.github.com>
2024-07-01 10:03:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,11 +7,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '99252742-58f6-40be-853f-c3d5dfc26a82';
+let userID = '1bd3e7b3-fb9f-410d-a8e1-cdfeff8f15de';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= ['c.cn5.us.kg'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
